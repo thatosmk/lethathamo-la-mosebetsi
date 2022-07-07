8246999f58aee01faef3a3e4df94b1721ac98e85
Thato Semoko <thatosmk@gmail.com>
2022-07-07 17:44:16 +0200

    adds tailwindcss file and configures brand colors

---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#5EA5EE',
+        secondary: '#343951',
+        'dark-grey': '#5C5C5C',
+        'light-accent': '#E4EBEF',
+        cultured: '#F3F4F5'
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
